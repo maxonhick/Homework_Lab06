9291dee0d98bf4ed1ac46002865e3f4dd0eb3985
maxonhick <max2007varlamov@gmail.com>
2025-05-24 21:20:13 +0300

    init lab06

---
+++ _build/CMakeFiles/solver.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/solver.dir/link.d"
+  "CMakeFiles/solver.dir/src/equation.cpp.o"
+  "CMakeFiles/solver.dir/src/equation.cpp.o.d"
+  "CMakeFiles/solver.dir/src/formatter_ex.cpp.o"
+  "CMakeFiles/solver.dir/src/formatter_ex.cpp.o.d"
+  "CMakeFiles/solver.dir/src/solver.cpp.o"
+  "CMakeFiles/solver.dir/src/solver.cpp.o.d"
+  "solver"
+  "solver.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/solver.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
